eea9c6db2523c0834807c4bebe839cf8a8d49e63
Pixeloza <100315088+Pix3loza@users.noreply.github.com>
2024-04-15 16:16:13 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,7 +2,6 @@ ESX = exports["es_extended"]:getSharedObject()
 
 RegisterNetEvent('pixel-jobcenter:Setjob', function (job, grade)
     local xPlayer = ESX.GetPlayerFromId(source)
-    print('nigger')
     if ESX.DoesJobExist(job, grade) then 
         xPlayer.setJob(job, grade)
     end
